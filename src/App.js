67a68213d98cb7d83054b598eda7f77ce9aadb41
caren1 <czarnocki.w@wp.pl>
2020-08-30 11:14:17 +0200

    added react-router-dom + routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import SearchPage from './components/SearchPage';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
+      
+      <Router>
       <Header />
-      <Home/>
-      <Footer />
+        <Switch>
+
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route path="/">
+            <Home/>
+          </Route>
+
+        </Switch>
+        <Footer />
+      </Router>
     
     {/* HOME */}
       {/* Header */}
